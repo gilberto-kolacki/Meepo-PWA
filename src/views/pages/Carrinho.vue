6bec2c4ceef7c9536a5720bcc40dc9d2b02ba286
Giba <gilbertokolacki@gmail.com>
2019-12-04 10:01:18 -0300

    Tela carrinho

--- src/views/pages/Carrinho.vue
+++ src/views/pages/Carrinho.vue
@@ -1,5 +1,61 @@
 <template>
+    <div>
+        {{JSON.stringify(this.carrinho)}}
+    </div>
 </template>
 
 <script>
+
+import Storage  from '../../rapidsoft/utils/storage'
+import ProdutoUtils  from '../../rapidsoft/utils/produtoUtils'
+
+export default {
+    data: () => ({
+        carrinho: null,
+        selected: [],
+        itemsPerPage: 4,
+        isMounted: false,
+        addNewDataSidebar: false,
+        sidebarData: {},
+
+    }),
+    components: {
+        
+    },
+    computed: {
+        currentPage() {
+            if(this.isMounted) {
+                return this.$refs.table.currentx
+            }
+            return 0
+        },
+        products() {
+            return this.carrinho;
+        },
+        queriedItems() {
+            return this.$refs.table ? this.$refs.table.queriedResults.length : this.products.length
+        }
+    },
+    methods: {
+        toggleDataSidebar(val=false) {
+            this.addNewDataSidebar = val
+        }
+
+
+    },
+    beforeCreate() {       
+        
+    },
+    created() {        
+        
+    },
+    mounted() {
+        ProdutoUtils.getCarrinho().then((carrinho) => {
+            this.carrinho = carrinho;
+            console.log(this.carrinho);
+        })
+        
+    }
+}
+
 </script>
