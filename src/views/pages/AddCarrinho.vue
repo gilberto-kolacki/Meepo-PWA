e210d3929571b025f9a7f7d985b36174289b4d9c
Cleiton Ramos <ramoscleitoneduardo@gmail.com>
2020-02-25 17:27:32 -0300

    Titulo complete look

--- src/views/pages/AddCarrinho.vue
+++ src/views/pages/AddCarrinho.vue
@@ -37,7 +37,7 @@
             >
             </add-carrinho-item>
         </div>
-        
+        <h2 class='mt-5' style="display:flex;justify-content: center;" v-if="this.produtosDoLook.length > 0">Complete o Look</h2>
         <div class='flex justify-center w-full' v-if="this.produtosDoLook.length > 0">
             <div class="produto-image-gallery vx-row mt-6" style="width:95%" id="content-produtos">
                 <div class="vx-col px-1 lg:w-1/4 md:w-1/4 sm:w-1/3 mb-4" v-for="(produtoLook, indextr) in this.produtosDoLook" :key="indextr">
